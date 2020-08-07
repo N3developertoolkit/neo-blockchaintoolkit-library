f76fc8876f5aff398abd47e5c86d06899b9c8cfb
Harry Pierson <harrypierson@ngd.neo.org>
2020-08-07 14:50:05 -0700

    add testapp

---
+++ test/trace-testapp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace trace_testapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
