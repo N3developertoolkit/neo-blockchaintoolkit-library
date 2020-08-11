947da575fd8a639dc8da1bc07c44388511abf43b
Harry Pierson <harrypierson@ngd.neo.org>
2020-08-11 14:38:21 -0700

    update global.json

--- test/trace-testapp/Program.cs
+++ test/trace-testapp/Program.cs
@@ -9,49 +9,15 @@ using Nerdbank.Streams;
 
 namespace trace_testapp
 {
-    class Program
+    internal static class Program
     {
-        const string PATH = @"C:\Users\harry\Source\neo\seattle\express\src\nxp3\0x85b99e4467e34df2308b7471c4335cee1c76b241d54c7dc5a61188e02eab4e1f.neo-trace";
-        static readonly MessagePackSerializerOptions options = MessagePackSerializerOptions.Standard
-            .WithResolver(TraceDebugResolver.Instance);
-
-        static void Foo(IBufferWriter<byte> bufferWriter, string msg)
+        private static void Main()
         {
-            var writer = new MessagePackWriter(bufferWriter);
-            writer.WriteArrayHeader(2);
-            writer.WriteInt32(2);
-            writer.WriteArrayHeader(2);
-            options.Resolver.GetFormatterWithVerify<Neo.UInt160>().Serialize(ref writer, Neo.UInt160.Zero, options);
-            options.Resolver.GetFormatterWithVerify<string>().Serialize(ref writer, msg, options);
-            writer.Flush();
-        }
+            const string PATH = "C:/Users/harry/Source/neo/seattle/express/src/nxp3/0xbb00986f67974c2277ffbe79b7c60981dc7348a0bd724e56644e562538af5ae6.neo-trace";
 
-        private static byte[] Foo()
-        {
-            var seq = new Sequence<byte>();
-            Foo(seq, "test one");
-            Foo(seq, "test two");
-            var roseq = seq.AsReadOnlySequence;
+            var options = MessagePackSerializerOptions.Standard
+                .WithResolver(TraceDebugResolver.Instance);
 
-            var stream = new MemoryStream();
-            foreach (var seg in roseq)
-            {
-                stream.Write(seg.Span);
-            }
-            return stream.ToArray();
-
-            // MessagePackSerializer.Serialize<ITraceRecord>(seq, new Log(Neo.UInt160.Zero, "test one"), options);
-            // MessagePackSerializer.Serialize<ITraceRecord>(seq, new Log(Neo.UInt160.Zero, "test two"), options);
-
-            // var roSeq = seq.AsReadOnlySequence;
-            // while (roSeq.Length > 0)
-            // {
-            //     var rec = MessagePackSerializer.Deserialize<ITraceRecord>(roSeq, options);
-            // }
-
-        }
-        private static async Task Main()
-        {
             using var stream = File.OpenRead(PATH);
 
             while (stream.Position < stream.Length)
@@ -61,6 +27,10 @@ namespace trace_testapp
                 if (rec is TraceRecord tp)
                 {
                     Console.WriteLine($"\t{tp.StackFrames.Count}");
+                    // if (tp.StackFrames.Count > 0)
+                    // {
+                    //     Console.WriteLine($"\t{tp.StackFrames[0].InstructionPointer}");
+                    // }
                 }
             }
         }
