1298adad2f190697e459d6db41cdc6e494afd9c1
Harry <harrypierson@ngd.neo.org>
2020-12-07 12:06:59 -0800

    add script name to log + notify records

--- src/bctklib-3/trace-models/LogRecord.cs
+++ src/bctklib-3/trace-models/LogRecord.cs
@@ -11,21 +11,25 @@ namespace Neo.BlockchainToolkit.TraceDebug
         [Key(0)]
         public readonly UInt160 ScriptHash;
         [Key(1)]
+        public readonly string ScriptName;
+        [Key(2)]
         public readonly string Message;
 
-        public LogRecord(UInt160 scriptHash, string message)
+        public LogRecord(UInt160 scriptHash, string scriptName, string message)
         {
             ScriptHash = scriptHash;
+            ScriptName = scriptName;
             Message = message;
         }
 
-        public static void Write(IBufferWriter<byte> writer, MessagePackSerializerOptions options, UInt160 scriptHash, string message)
+        public static void Write(IBufferWriter<byte> writer, MessagePackSerializerOptions options, UInt160 scriptHash, string scriptName, string message)
         {
             var mpWriter = new MessagePackWriter(writer);
             mpWriter.WriteArrayHeader(2);
             mpWriter.WriteInt32(RecordKey);
-            mpWriter.WriteArrayHeader(2);
+            mpWriter.WriteArrayHeader(3);
             options.Resolver.GetFormatterWithVerify<Neo.UInt160>().Serialize(ref mpWriter, scriptHash, options);
+            options.Resolver.GetFormatterWithVerify<string>().Serialize(ref mpWriter, scriptName, options);
             options.Resolver.GetFormatterWithVerify<string>().Serialize(ref mpWriter, message, options);
             mpWriter.Flush();
         }
