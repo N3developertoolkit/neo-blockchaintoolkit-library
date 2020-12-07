5c68b4a12eadfab6a0ef5fc44017b0bdbefba35f
Harry <harrypierson@ngd.neo.org>
2020-12-07 13:02:07 -0800

    fix tests

--- test/test.bctklib-3/ContractParameterParserTest.cs
+++ test/test.bctklib-3/ContractParameterParserTest.cs
@@ -18,10 +18,8 @@ namespace test.bctklib3
         public void TestParseStringParameter_string()
         {
             const string expected = "string-value";
-            var fileSystem = new MockFileSystem();
-            var accounts = new Dictionary<string, UInt160>();
-            var parser = new ContractParameterParser(fileSystem, accounts.TryGetValue);
-            var param = parser.ParseStringParameter(expected, string.Empty);
+            var parser = new ContractParameterParser(null, null);
+            var param = parser.ParseStringParameter(expected);
             param.Type.ShouldBe(ContractParameterType.String);
             param.Value.ShouldBe(expected);
         }
@@ -31,13 +29,12 @@ namespace test.bctklib3
         {
             const string account = "test-account";
             var expectedValue = UInt160.Parse("30f41a14ca6019038b055b585d002b287b5fdd47");
-            var fileSystem = new MockFileSystem();
             var accounts = new Dictionary<string, UInt160>
             {
                 { account, expectedValue }
             };
-            var parser = new ContractParameterParser(fileSystem, accounts.TryGetValue);
-            var param = parser.ParseStringParameter($"@{account}", string.Empty);
+            var parser = new ContractParameterParser(tryGetAccount: accounts.TryGetValue);
+            var param = parser.ParseStringParameter($"@{account}");
             param.Type.ShouldBe(ContractParameterType.Hash160);
             param.Value.ShouldBe(expectedValue);
         }
@@ -46,10 +43,8 @@ namespace test.bctklib3
         public void TestParseStringParameter_at_account_missing()
         {
             const string account = "test-account";
-            var fileSystem = new MockFileSystem();
-            var accounts = new Dictionary<string, UInt160>();
-            var parser = new ContractParameterParser(fileSystem, accounts.TryGetValue);
-            var param = parser.ParseStringParameter($"@{account}", string.Empty);
+            var parser = new ContractParameterParser(null, null);
+            var param = parser.ParseStringParameter($"@{account}");
             param.Type.ShouldBe(ContractParameterType.String);
             param.Value.ShouldBe($"@{account}");
         }
@@ -59,10 +54,8 @@ namespace test.bctklib3
         {
             var expectedValue = UInt160.Parse("30f41a14ca6019038b055b585d002b287b5fdd47");
             var address = Neo.Wallets.Helper.ToAddress(expectedValue);
-            var fileSystem = new MockFileSystem();
-            var accounts = new Dictionary<string, UInt160>();
-            var parser = new ContractParameterParser(fileSystem, accounts.TryGetValue);
-            var param = parser.ParseStringParameter($"@{address}", string.Empty);
+            var parser = new ContractParameterParser(null, null);
+            var param = parser.ParseStringParameter($"@{address}");
             param.Type.ShouldBe(ContractParameterType.Hash160);
             param.Value.ShouldBe(expectedValue);
         }
@@ -75,10 +68,8 @@ namespace test.bctklib3
             var address = Neo.Wallets.Helper.ToAddress(uint160);
             var expected = "@" + address.Substring(0, address.Length - 1);
 
-            var fileSystem = new MockFileSystem();
-            var accounts = new Dictionary<string, UInt160>();
-            var parser = new ContractParameterParser(fileSystem, accounts.TryGetValue);
-            var param = parser.ParseStringParameter(expected, string.Empty);
+            var parser = new ContractParameterParser(null, null);
+            var param = parser.ParseStringParameter(expected);
             param.Type.ShouldBe(ContractParameterType.String);
             param.Value.ShouldBe(expected);
         }
@@ -87,10 +78,8 @@ namespace test.bctklib3
         public void TestParseStringParameter_hash_uint160()
         {
             const string hashString = "30f41a14ca6019038b055b585d002b287b5fdd47";
-            var fileSystem = new MockFileSystem();
-            var accounts = new Dictionary<string, UInt160>();
-            var parser = new ContractParameterParser(fileSystem, accounts.TryGetValue);
-            var param = parser.ParseStringParameter($"#{hashString}", string.Empty);
+            var parser = new ContractParameterParser(null, null);
+            var param = parser.ParseStringParameter($"#{hashString}");
             param.Type.ShouldBe(ContractParameterType.Hash160);
             param.Value.ShouldBe(UInt160.Parse(hashString));
         }
@@ -99,10 +88,8 @@ namespace test.bctklib3
         public void TestParseStringParameter_hash_uint160_fail()
         {
             const string hashString = "#30f41a14ca6019038b055b585d002b287b5fdd4";
-            var fileSystem = new MockFileSystem();
-            var accounts = new Dictionary<string, UInt160>();
-            var parser = new ContractParameterParser(fileSystem, accounts.TryGetValue);
-            var param = parser.ParseStringParameter(hashString, string.Empty);
+            var parser = new ContractParameterParser(null, null);
+            var param = parser.ParseStringParameter(hashString);
             param.Type.ShouldBe(ContractParameterType.String);
             param.Value.ShouldBe(hashString);
         }
@@ -111,10 +98,8 @@ namespace test.bctklib3
         public void TestParseStringParameter_hash_uint256()
         {
             const string hashString = "0a372ac8f778eeebb1ccdbb250fe596b83d1d1b9f366d71dfd4c53956bed5cce";
-            var fileSystem = new MockFileSystem();
-            var accounts = new Dictionary<string, UInt160>();
-            var parser = new ContractParameterParser(fileSystem, accounts.TryGetValue);
-            var param = parser.ParseStringParameter($"#{hashString}", string.Empty);
+            var parser = new ContractParameterParser(null, null);
+            var param = parser.ParseStringParameter($"#{hashString}");
             param.Type.ShouldBe(ContractParameterType.Hash256);
             param.Value.ShouldBe(UInt256.Parse(hashString));
         }
@@ -123,10 +108,8 @@ namespace test.bctklib3
         public void TestParseStringParameter_hash_uint256_fail()
         {
             const string hashString = "#a372ac8f778eeebb1ccdbb250fe596b83d1d1b9f366d71dfd4c53956bed5cce";
-            var fileSystem = new MockFileSystem();
-            var accounts = new Dictionary<string, UInt160>();
-            var parser = new ContractParameterParser(fileSystem, accounts.TryGetValue);
-            var param = parser.ParseStringParameter(hashString, string.Empty);
+            var parser = new ContractParameterParser(null, null);
+            var param = parser.ParseStringParameter(hashString);
             param.Type.ShouldBe(ContractParameterType.String);
             param.Value.ShouldBe(hashString);
         }
@@ -137,86 +120,28 @@ namespace test.bctklib3
                 ? @"x:\fakepath" : "/fakepath";
         }
 
-        // [Fact]
-        // public void TestParseStringParameter_hash_script_absolute()
-        // {
-        //     var nef = new NefFile
-        //     {
-        //         Compiler = "".PadLeft(32, ' '),
-        //         Version = new Version(1, 2, 3, 4),
-        //         Script = new byte[] { 0x01, 0x02, 0x03 }
-        //     };
-        //     nef.ScriptHash = nef.Script.ToScriptHash();
-        //     nef.CheckSum = NefFile.ComputeChecksum(nef);
+        [Fact]
+        public void TestParseStringParameter_hash_script()
+        {
+            const string contractName = "test-contract";
+            var expectedValue = UInt160.Parse("30f41a14ca6019038b055b585d002b287b5fdd47");
 
-        //     var fileSystem = new MockFileSystem();
-        //     var nefPath = fileSystem.Path.Combine(FakeRootPath(), "contract.nef");
-        //     fileSystem.AddFile(nefPath, new MockFileData(nef.ToArray()));
+            var contracts = new Dictionary<string, UInt160>()
+            {
+                { contractName, expectedValue }
+            };
 
-        //     var accounts = new Dictionary<string, UInt160>();
-        //     var parser = new ContractParameterParser(fileSystem, accounts.TryGetValue);
-        //     var param = parser.ParseStringParameter($"#{nefPath}", string.Empty);
-        //     param.Type.ShouldBe(ContractParameterType.Hash160);
-        //     param.Value.ShouldBe(nef.ScriptHash);
-        // }
-
-        // [Fact]
-        // public void TestParseStringParameter_hash_script_relative()
-        // {
-        //     var nef = new NefFile
-        //     {
-        //         Compiler = "".PadLeft(32, ' '),
-        //         Version = new Version(1, 2, 3, 4),
-        //         Script = new byte[] { 0x01, 0x02, 0x03 }
-        //     };
-        //     nef.ScriptHash = nef.Script.ToScriptHash();
-        //     nef.CheckSum = NefFile.ComputeChecksum(nef);
-
-        //     var fileSystem = new MockFileSystem();
-        //     var rootPath = FakeRootPath();
-        //     var nefPath = fileSystem.Path.Combine(rootPath, "contract.nef");
-        //     fileSystem.AddFile(nefPath, new MockFileData(nef.ToArray()));
-
-        //     var relativePath = fileSystem.Path.GetRelativePath(rootPath, nefPath);
-
-        //     var accounts = new Dictionary<string, UInt160>();
-        //     var parser = new ContractParameterParser(fileSystem, accounts.TryGetValue);
-        //     var param = parser.ParseStringParameter($"#{relativePath}", rootPath);
-        //     param.Type.ShouldBe(ContractParameterType.Hash160);
-        //     param.Value.ShouldBe(nef.ScriptHash);
-        // }
-
-        // [Fact]
-        // public void TestParseStringParameter_hash_script_relative_invalid()
-        // {
-        //     const string nefPath = @"x:\fakepath\contract.nef";
-        //     var nef = new NefFile
-        //     {
-        //         Compiler = "".PadLeft(32, ' '),
-        //         Version = new Version(1, 2, 3, 4),
-        //         Script = new byte[] { 0x01, 0x02, 0x03 }
-        //     };
-        //     nef.ScriptHash = nef.Script.ToScriptHash();
-        //     nef.CheckSum = NefFile.ComputeChecksum(nef);
-
-        //     var fileSystem = new MockFileSystem();
-        //     fileSystem.AddFile(nefPath, new MockFileData(nef.ToArray()));
-
-        //     var accounts = new Dictionary<string, UInt160>();
-        //     var parser = new ContractParameterParser(fileSystem, accounts.TryGetValue);
-
-        //     var param = parser.ParseStringParameter("#contract.nef", string.Empty);
-        //     param.Type.ShouldBe(ContractParameterType.String);
-        //     param.Value.ShouldBe("#contract.nef");
-        // }
+            var parser = new ContractParameterParser(tryGetContract: contracts.TryGetValue);
+            var param = parser.ParseStringParameter($"#{contractName}");
+            param.Type.ShouldBe(ContractParameterType.Hash160);
+            param.Value.ShouldBe(expectedValue);
+        }
 
         [Fact]
         public void TestParseStringParameter_hash_script_native()
         {
-            var fileSystem = new MockFileSystem();
-            var accounts = new Dictionary<string, UInt160>();
-            var parser = new ContractParameterParser(fileSystem, accounts.TryGetValue);
-            var param = parser.ParseStringParameter("#oracle", string.Empty);
+            var parser = new ContractParameterParser(null, null);
+            var param = parser.ParseStringParameter("#oracle");
             param.Type.ShouldBe(ContractParameterType.Hash160);
             param.Value.ShouldBe(Neo.SmartContract.Native.NativeContract.Oracle.Hash);
         }
@@ -234,10 +159,8 @@ namespace test.bctklib3
                 0x19, 0x1a, 0x1b, 0x1c,
                 0x1d, 0x1e, 0x1f, 0x20 };
 
-            var fileSystem = new MockFileSystem();
-            var accounts = new Dictionary<string, UInt160>();
-            var parser = new ContractParameterParser(fileSystem, accounts.TryGetValue);
-            var param = parser.ParseStringParameter($"0x{expectedValue.ToHexString()}", string.Empty);
+            var parser = new ContractParameterParser(null, null);
+            var param = parser.ParseStringParameter($"0x{expectedValue.ToHexString()}");
             param.Type.ShouldBe(ContractParameterType.ByteArray);
             param.Value.ShouldBe(expectedValue);
         }
