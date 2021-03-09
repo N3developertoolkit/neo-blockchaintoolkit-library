574c5f278c1eac832a125cadb8749b37ae29d2fa
Harry Pierson <harrypierson@hotmail.com>
2021-03-09 09:22:35 -0800

    GetProtocolSettings + ExpressChain.Settings (#8)
    * GetProtocolSettings
    
    * ExpressChain.Settings
    
    Co-authored-by: Harry <harrypierson@outlook.com>

--- src/bctklib-3/Extensions.cs
+++ src/bctklib-3/Extensions.cs
@@ -1,13 +1,35 @@
 
 using System;
 using System.IO.Abstractions;
+using System.Linq;
 using Neo.BlockchainToolkit.Models;
+using Neo.Cryptography.ECC;
+using Neo.Wallets;
 using Newtonsoft.Json;
 
 namespace Neo.BlockchainToolkit
 {
     public static class Extensions
     {
+        public static ProtocolSettings GetProtocolSettings(this ExpressChain? chain, uint secondsPerBlock = 0)
+        {
+            return chain == null 
+                ? ProtocolSettings.Default 
+                : ProtocolSettings.Default with {
+                    Magic = chain.Magic,
+                    AddressVersion = chain.AddressVersion,
+                    MillisecondsPerBlock = secondsPerBlock == 0 ? 15000 : secondsPerBlock * 1000,
+                    ValidatorsCount = chain.ConsensusNodes.Count,
+                    StandbyCommittee = chain.ConsensusNodes.Select(GetPublicKey).ToArray(),
+                    SeedList = chain.ConsensusNodes
+                        .Select(n => $"{System.Net.IPAddress.Loopback}:{n.TcpPort}")
+                        .ToArray(),
+                };
+
+            static ECPoint GetPublicKey(ExpressConsensusNode node)
+                => new KeyPair(node.Wallet.Accounts.Select(a => a.PrivateKey).Distinct().Single().HexToBytes()).PublicKey;
+        }
+
         public static ExpressChain LoadChain(this IFileSystem fileSystem, string path)
         {
             var serializer = new JsonSerializer();
