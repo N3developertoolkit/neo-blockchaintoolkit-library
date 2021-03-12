b0ad49f368363c33594eafaa94793809e265a10d
Harry <harrypierson@outlook.com>
2021-03-11 16:25:55 -0800

    Constants

---
+++ src/bctklib-3/Constants.cs
@@ -0,0 +1,8 @@
+namespace Neo.BlockchainToolkit
+{
+    public static class Constants
+    {
+        public const string EXPRESS_EXTENSION = ".neo-express";
+        public const string DEFAULT_EXPRESS_FILENAME = "default" + EXPRESS_EXTENSION;
+    }
+}
