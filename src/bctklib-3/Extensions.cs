dafbb986bb5cf596e5aa5d9abf3e29b24a58b143
Harry <harrypierson@outlook.com>
2021-04-19 16:08:25 -0700

    GetOperandString + GetComment

--- src/bctklib-3/Extensions.cs
+++ src/bctklib-3/Extensions.cs
@@ -1,6 +1,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Diagnostics.CodeAnalysis;
 using System.IO.Abstractions;
 using System.Linq;
@@ -100,6 +101,89 @@ namespace Neo.BlockchainToolkit
             }
         }
 
+        public static string GetOperandString(this Instruction instruction)
+        {
+            return string.Create<ReadOnlyMemory<byte>>(instruction.Operand.Length * 3 - 1,
+                instruction.Operand, (span, memory) =>
+                {
+                    var first = memory.Span[0];
+                    span[0] = GetHexValue(first / 16);
+                    span[1] = GetHexValue(first % 16);
+
+                    var index = 1;
+                    for (var i = 2; i < span.Length; i += 3)
+                    {
+                        var b = memory.Span[index++];
+                        span[i] = '-';
+                        span[i + 1] = GetHexValue(b / 16);
+                        span[i + 2] = GetHexValue(b % 16);
+                    }
+                });
+
+            static char GetHexValue(int i) => (i < 10) ? (char)(i + '0') : (char)(i - 10 + 'A');
+        }
+
+        public static string GetComment(this Instruction instruction, int ip, IReadOnlyDictionary<uint, string>? sysCallNames = null)
+        {
+            sysCallNames ??= ImmutableDictionary<uint, string>.Empty;
+
+            switch (instruction.OpCode)
+            {
+                case OpCode.PUSHINT8:
+                case OpCode.PUSHINT16:
+                case OpCode.PUSHINT32:
+                case OpCode.PUSHINT64:
+                case OpCode.PUSHINT128:
+                case OpCode.PUSHINT256:
+                    return $"{new System.Numerics.BigInteger(instruction.Operand.Span)}";
+                case OpCode.PUSHM1:
+                    return $"{(int)instruction.OpCode - (int)OpCode.PUSH0}";
+                case OpCode.PUSHDATA1:
+                case OpCode.PUSHDATA2:
+                case OpCode.PUSHDATA4:
+                    {
+                        var text = System.Text.Encoding.UTF8.GetString(instruction.Operand.Span)
+                            .Replace("\r", "\"\\r\"").Replace("\n", "\"\\n\"");
+                        if (instruction.Operand.Length == 20)
+                        {
+                            return $"as script hash: {new Neo.UInt160(instruction.Operand.Span)}, as text: \"{text}\"";
+                        }
+                        return $"as text: \"{text}\"";
+                    }
+                case OpCode.SYSCALL:
+                    return sysCallNames.TryGetValue(instruction.TokenU32, out var name)
+                        ? name
+                        : $"Unknown SysCall {instruction.TokenU32}";
+                case OpCode.INITSLOT:
+                    return $"{instruction.TokenU8} local variables, {instruction.TokenU8_1} arguments";
+                case OpCode.JMP_L:
+                case OpCode.JMPEQ_L:
+                case OpCode.JMPGE_L:
+                case OpCode.JMPGT_L:
+                case OpCode.JMPIF_L:
+                case OpCode.JMPIFNOT_L:
+                case OpCode.JMPLE_L:
+                case OpCode.JMPLT_L:
+                case OpCode.JMPNE_L:
+                case OpCode.CALL_L:
+                    return OffsetComment(instruction.TokenI32);
+                case OpCode.JMP:
+                case OpCode.JMPEQ:
+                case OpCode.JMPGE:
+                case OpCode.JMPGT:
+                case OpCode.JMPIF:
+                case OpCode.JMPIFNOT:
+                case OpCode.JMPLE:
+                case OpCode.JMPLT:
+                case OpCode.JMPNE:
+                case OpCode.CALL:
+                    return OffsetComment(instruction.TokenI8);
+                default:
+                    return string.Empty;
+            }
+
+            string OffsetComment(int offset) => $"pos: {ip + offset}, offset: {offset}";
+        }
 
         public static TestApplicationEngine GetTestApplicationEngine(this ExpressChain chain, DataCache snapshot)
             => new TestApplicationEngine(snapshot, chain.GetProtocolSettings());
