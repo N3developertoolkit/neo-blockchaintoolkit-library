6acdd10bd993635a55e3f65d03dcae8092e203ca
Harry <harrypierson@outlook.com>
2021-04-19 16:50:31 -0700

    assorted cleanup

--- src/bctklib-3/Extensions.cs
+++ src/bctklib-3/Extensions.cs
@@ -5,11 +5,14 @@ using System.Collections.Immutable;
 using System.Diagnostics.CodeAnalysis;
 using System.IO.Abstractions;
 using System.Linq;
+using System.Numerics;
 using Neo.BlockchainToolkit.Models;
 using Neo.Cryptography.ECC;
+using Neo.SmartContract;
 using Neo.VM;
 using Neo.Wallets;
 using Newtonsoft.Json;
+using JsonSerializer = Newtonsoft.Json.JsonSerializer;
 
 namespace Neo.BlockchainToolkit
 {
@@ -79,6 +82,50 @@ namespace Neo.BlockchainToolkit
             return false;
         }
 
+        public static ExpressWallet GetWallet(this ExpressChain chain, string name)
+            => TryGetWallet(chain, name, out var wallet)
+                ? wallet
+                : throw new Exception($"wallet {name} not found");
+
+        public static bool TryGetWallet(this ExpressChain chain, string name, [NotNullWhen(true)] out ExpressWallet? wallet)
+        {
+            for (int i = 0; i < chain.Wallets.Count; i++)
+            {
+                if (string.Equals(name, chain.Wallets[i].Name, StringComparison.OrdinalIgnoreCase))
+                {
+                    wallet = chain.Wallets[i];
+                    return true;
+                }
+            }
+
+            wallet = null;
+            return false;
+        }
+
+        public static ExpressWalletAccount GetDefaultAccount(this ExpressChain chain, string name)
+            => TryGetDefaultAccount(chain, name, out var account)
+                ? account
+                : throw new Exception($"default account for {name} wallet not found");
+
+        public static UInt160 GetDefaultAccountScriptHash(this ExpressChain chain, string name)
+            => TryGetDefaultAccount(chain, name, out var account)
+                ? account.ToScriptHash(chain.AddressVersion)
+                : throw new Exception($"default account for {name} wallet not found");
+
+        public static bool TryGetDefaultAccount(this ExpressChain chain, string name, [NotNullWhen(true)] out ExpressWalletAccount? account)
+        {
+            if (chain.TryGetWallet(name, out var wallet) && wallet.DefaultAccount != null)
+            {
+                account = wallet.DefaultAccount;
+                return true;
+            }
+
+            account = null;
+            return false;
+        }
+        public static UInt160 ToScriptHash(this ExpressWalletAccount account, byte addressVersion)
+            => account.ScriptHash.ToScriptHash(addressVersion);
+
         public static string GetInstructionAddressPadding(this Script script)
         {
             var digitCount = EnumerateInstructions(script).Last().address switch
@@ -87,8 +134,8 @@ namespace Neo.BlockchainToolkit
                 var x when x < 100 => 2,
                 var x when x < 1000 => 3,
                 var x when x < 10000 => 4,
-                var x when x < 100000 => 5,
-                _ => throw new Exception("Max script length is 65536 bytes"),
+                var x when x <= ushort.MaxValue => 5,
+                _ => throw new Exception($"Max script length is {ushort.MaxValue} bytes"),
             };
             return new string('0', digitCount);
         }
@@ -134,7 +181,7 @@ namespace Neo.BlockchainToolkit
         }
 
         static readonly Lazy<IReadOnlyDictionary<uint, string>> sysCallNames = new Lazy<IReadOnlyDictionary<uint, string>>(
-            () => Neo.SmartContract.ApplicationEngine.Services.ToImmutableDictionary(kvp => kvp.Value.Hash, kvp => kvp.Value.Name));
+            () => ApplicationEngine.Services.ToImmutableDictionary(kvp => kvp.Value.Hash, kvp => kvp.Value.Name));
 
         public static string GetComment(this Instruction instruction, int ip)
         {
@@ -146,7 +193,7 @@ namespace Neo.BlockchainToolkit
                 case OpCode.PUSHINT64:
                 case OpCode.PUSHINT128:
                 case OpCode.PUSHINT256:
-                    return $"{new System.Numerics.BigInteger(instruction.Operand.Span)}";
+                    return $"{new BigInteger(instruction.Operand.Span)}";
                 case OpCode.PUSHM1:
                     return $"{(int)instruction.OpCode - (int)OpCode.PUSH0}";
                 case OpCode.PUSHDATA1:
@@ -157,7 +204,7 @@ namespace Neo.BlockchainToolkit
                             .Replace("\r", "\"\\r\"").Replace("\n", "\"\\n\"");
                         if (instruction.Operand.Length == 20)
                         {
-                            return $"as script hash: {new Neo.UInt160(instruction.Operand.Span)}, as text: \"{text}\"";
+                            return $"as script hash: {new UInt160(instruction.Operand.Span)}, as text: \"{text}\"";
                         }
                         return $"as text: \"{text}\"";
                     }
@@ -195,49 +242,5 @@ namespace Neo.BlockchainToolkit
 
             string OffsetComment(int offset) => $"pos: {ip + offset}, offset: {offset}";
         }
-
-        public static ExpressWallet GetWallet(this ExpressChain chain, string name)
-            => TryGetWallet(chain, name, out var wallet)
-                ? wallet
-                : throw new Exception($"wallet {name} not found");
-
-        public static bool TryGetWallet(this ExpressChain chain, string name, [NotNullWhen(true)] out ExpressWallet? wallet)
-        {
-            for (int i = 0; i < chain.Wallets.Count; i++)
-            {
-                if (string.Equals(name, chain.Wallets[i].Name, StringComparison.OrdinalIgnoreCase))
-                {
-                    wallet = chain.Wallets[i];
-                    return true;
-                }
-            }
-
-            wallet = null;
-            return false;
-        }
-
-        public static ExpressWalletAccount GetDefaultAccount(this ExpressChain chain, string name)
-            => TryGetDefaultAccount(chain, name, out var account)
-                ? account
-                : throw new Exception($"default account for {name} wallet not found");
-
-        public static UInt160 GetDefaultAccountScriptHash(this ExpressChain chain, string name)
-            => TryGetDefaultAccount(chain, name, out var account)
-                ? account.ToScriptHash(chain.AddressVersion)
-                : throw new Exception($"default account for {name} wallet not found");
-
-        public static bool TryGetDefaultAccount(this ExpressChain chain, string name, [NotNullWhen(true)] out ExpressWalletAccount? account)
-        {
-            if (chain.TryGetWallet(name, out var wallet) && wallet.DefaultAccount != null)
-            {
-                account = wallet.DefaultAccount;
-                return true;
-            }
-
-            account = null;
-            return false;
-        }
-        public static UInt160 ToScriptHash(this ExpressWalletAccount account, byte addressVersion)
-            => account.ScriptHash.ToScriptHash(addressVersion);
     }
 }
