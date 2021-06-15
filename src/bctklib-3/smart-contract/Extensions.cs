85426aaf6f43c3d01feda5ea62eadc9e423facf3
Harry Pierson <harrypierson@hotmail.com>
2021-06-15 14:04:46 -0700

    * Add CreateTestTransaction helpers (#33)
    * Use CreateTestTransaction as appropriate in TestAppEngine ctor overloads
    * Add TestAppEngine ctor that accepts a tx instance
    
    Co-authored-by: Harry <harrypierson@outlook.com>

--- src/bctklib-3/smart-contract/Extensions.cs
+++ src/bctklib-3/smart-contract/Extensions.cs
@@ -11,8 +11,11 @@ namespace Neo.BlockchainToolkit.SmartContract
         public static TestApplicationEngine GetTestApplicationEngine(this ExpressChain chain, DataCache snapshot)
             => new TestApplicationEngine(snapshot, chain.GetProtocolSettings());
 
-        public static TestApplicationEngine GetTestApplicationEngine(this ExpressChain chain, DataCache snapshot, UInt160 signer)
-            => new TestApplicationEngine(snapshot, chain.GetProtocolSettings(), signer);
+        public static TestApplicationEngine GetTestApplicationEngine(this ExpressChain chain, DataCache snapshot, UInt160 signer, WitnessScope witnessScope = WitnessScope.CalledByEntry)
+            => new TestApplicationEngine(snapshot, chain.GetProtocolSettings(), signer, witnessScope);
+
+        public static TestApplicationEngine GetTestApplicationEngine(this ExpressChain chain, DataCache snapshot, Transaction transaction)
+            => new TestApplicationEngine(snapshot, chain.GetProtocolSettings(), transaction);
 
         public static TestApplicationEngine GetTestApplicationEngine(this ExpressChain chain, TriggerType trigger, IVerifiable? container, DataCache snapshot, Block? persistingBlock, long gas, Func<byte[], bool>? witnessChecker)
             => new TestApplicationEngine(trigger, container, snapshot, persistingBlock, chain.GetProtocolSettings(), gas, witnessChecker);
