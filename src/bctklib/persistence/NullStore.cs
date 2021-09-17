09cd6380cecdbb8710e7ebdb90c4c15e4968a691
Harry <harrypierson@outlook.com>
2021-09-16 17:35:22 -0700

    move nullstore out of CheckpointStorageProvider

---
+++ src/bctklib/persistence/NullStore.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using Neo.Persistence;
+using OneOf;
+
+namespace Neo.BlockchainToolkit.Persistence
+{
+    class NullStore : IReadOnlyStore
+    {
+        public static NullStore Instance = new NullStore();
+
+        private NullStore() { }
+
+        public bool Contains(byte[]? key) => false;
+        public byte[]? TryGet(byte[]? key) => null;
+        public IEnumerable<(byte[] Key, byte[]? Value)> Seek(byte[] key, SeekDirection direction)
+            => Enumerable.Empty<(byte[], byte[]?)>();
+    }
+}
