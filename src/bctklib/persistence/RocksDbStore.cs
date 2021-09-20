053b0127247917795f898e33728136d077593136
Harry <harrypierson@outlook.com>
2021-09-20 15:19:28 -0700

    RocksDbStore

---
+++ src/bctklib/persistence/RocksDbStore.cs
@@ -0,0 +1,105 @@
+using System;
+using System.Collections.Generic;
+using Neo.Persistence;
+using RocksDbSharp;
+
+namespace Neo.BlockchainToolkit.Persistence
+{
+    public partial class RocksDbStore : IStore
+    {
+        readonly RocksDb db;
+        readonly ColumnFamilyHandle columnFamily;
+        readonly bool readOnly;
+        readonly bool shared;
+        bool disposed;
+        readonly ReadOptions readOptions = new ReadOptions();
+        readonly WriteOptions writeOptions = new WriteOptions();
+        readonly WriteOptions writeSyncOptions = new WriteOptions().SetSync(true);
+
+        internal RocksDbStore(RocksDb db, ColumnFamilyHandle columnFamily, bool readOnly, bool shared)
+        {
+            this.db = db;
+            this.columnFamily = columnFamily;
+            this.readOnly = readOnly;
+            this.shared = shared;
+        }
+
+        internal static RocksDb OpenDb(string path)
+        {
+            var columnFamilies = GetColumnFamilies(path);
+            return RocksDb.Open(new DbOptions().SetCreateIfMissing(true), path, columnFamilies);
+        }
+
+        internal static RocksDb OpenReadOnlyDb(string path)
+        {
+            var columnFamilies = GetColumnFamilies(path);
+            return RocksDb.OpenReadOnly(new DbOptions(), path, columnFamilies, false);
+        }
+
+        internal static ColumnFamilies GetColumnFamilies(string path)
+        {
+            if (RocksDb.TryListColumnFamilies(new DbOptions(), path, out var names))
+            {
+                var columnFamilyOptions = new ColumnFamilyOptions();
+                var families = new ColumnFamilies();
+                foreach (var name in names)
+                {
+                    families.Add(name, columnFamilyOptions);
+                }
+                return families;
+            }
+
+            return new ColumnFamilies();
+        }
+
+        public void Dispose()
+        {
+            disposed = true;
+            if (!shared) db.Dispose();
+        }
+
+        public byte[]? TryGet(byte[]? key)
+        {
+            if (disposed) throw new ObjectDisposedException(nameof(RocksDbStore));
+            return db.Get(key ?? Array.Empty<byte>(), columnFamily, readOptions);
+        }
+
+        public bool Contains(byte[]? key)
+        {
+            return TryGet(key) != null;
+        }
+
+        public IEnumerable<(byte[] Key, byte[] Value)> Seek(byte[]? key, SeekDirection direction)
+        {
+            if (disposed) throw new ObjectDisposedException(nameof(RocksDbStore));
+            return db.Seek(columnFamily, key, direction, readOptions);
+        }
+
+        public void Put(byte[]? key, byte[] value)
+        {
+            if (disposed) throw new ObjectDisposedException(nameof(RocksDbStore));
+            if (readOnly) throw new InvalidOperationException("read only");
+            db.Put(key ?? Array.Empty<byte>(), value, columnFamily, writeOptions);
+        }
+
+        public void PutSync(byte[]? key, byte[] value)
+        {
+            if (disposed) throw new ObjectDisposedException(nameof(RocksDbStore));
+            if (readOnly) throw new InvalidOperationException("read only");
+            db.Put(key ?? Array.Empty<byte>(), value, columnFamily, writeSyncOptions);
+        }
+
+        public void Delete(byte[]? key)
+        {
+            if (disposed) throw new ObjectDisposedException(nameof(RocksDbStore));
+            if (readOnly) throw new InvalidOperationException("read only");
+            db.Remove(key ?? Array.Empty<byte>(), columnFamily, writeOptions);
+        }
+
+        public ISnapshot GetSnapshot()
+        {
+            if (disposed) throw new ObjectDisposedException(nameof(RocksDbStore));
+            return new Snapshot(db, columnFamily);
+        }
+    }
+}
---
+++ test/test.bctklib/RocksDbStoreTest.cs
@@ -0,0 +1,60 @@
+using System;
+using Neo.BlockchainToolkit.Persistence;
+using Xunit;
+
+namespace test.bctklib3
+{
+    public class RocksDbStoreTest
+    {
+        [Fact]
+        public void can_put_and_get()
+        {
+            var path = Utility.GetTempPath();
+            using var _ = Utility.GetDeleteDirectoryDisposable(path);
+
+            {
+                var db = RocksDbStore.OpenDb(path);
+                using var store = new RocksDbStore(db, db.GetDefaultColumnFamily(), false, false);
+                store.Put(new byte[] { 0x00, 0x03 }, BitConverter.GetBytes(3));
+                store.Put(new byte[] { 0x00, 0x04 }, BitConverter.GetBytes(4));
+                store.Put(new byte[] { 0x01, 0x03 }, BitConverter.GetBytes(13));
+                store.Put(new byte[] { 0x01, 0x04 }, BitConverter.GetBytes(14));
+                store.Put(new byte[] { 0x02, 0x03 }, BitConverter.GetBytes(23));
+                store.Put(new byte[] { 0x02, 0x04 }, BitConverter.GetBytes(24));
+            }
+
+            {
+                var db = RocksDbStore.OpenReadOnlyDb(path);
+                using var store = new RocksDbStore(db, db.GetDefaultColumnFamily(), true, false);
+                var actual = store.TryGet(new byte[] { 0x00, 0x03 });
+                Assert.Equal<byte[]>(BitConverter.GetBytes(3), actual!);
+            }
+        }
+
+        [Fact]
+        public void readonly_store_throws_on_put_and_delete()
+        {
+            var path = Utility.GetTempPath();
+            using var _ = Utility.GetDeleteDirectoryDisposable(path);
+
+            {
+                var db = RocksDbStore.OpenDb(path);
+                using var store = new RocksDbStore(db, db.GetDefaultColumnFamily(), false, false);
+                store.Put(new byte[] { 0x00, 0x03 }, BitConverter.GetBytes(3));
+                store.Put(new byte[] { 0x00, 0x04 }, BitConverter.GetBytes(4));
+                store.Put(new byte[] { 0x01, 0x03 }, BitConverter.GetBytes(13));
+                store.Put(new byte[] { 0x01, 0x04 }, BitConverter.GetBytes(14));
+                store.Put(new byte[] { 0x02, 0x03 }, BitConverter.GetBytes(23));
+                store.Put(new byte[] { 0x02, 0x04 }, BitConverter.GetBytes(24));
+            }
+
+            {
+                var db = RocksDbStore.OpenReadOnlyDb(path);
+                using var store = new RocksDbStore(db, db.GetDefaultColumnFamily(), true, false);
+
+                Assert.Throws<InvalidOperationException>(() => store.Put(new byte[] { 0x04, 0x03 }, BitConverter.GetBytes(43)));
+                Assert.Throws<InvalidOperationException>(() => store.Delete(new byte[] { 0x00, 0x03 }));
+            }
+        }
+    }
+}
