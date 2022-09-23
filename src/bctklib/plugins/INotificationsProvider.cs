680356f149e41552c6d11e59c363b0a63fc94732
Harry Pierson <harrypierson@hotmail.com>
2022-09-23 09:40:57 -0700

    Move shared toolkit plugins to lib-bctk (#72)
    Co-authored-by: Harry <harrypierson@outlook.com>

---
+++ src/bctklib/plugins/INotificationsProvider.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using Neo.Persistence;
+
+namespace Neo.BlockchainToolkit.Plugins
+{
+    public readonly record struct NotificationInfo(uint BlockIndex, 
+        ushort TxIndex,
+        ushort NotificationIndex,
+        NotificationRecord Notification);
+
+    interface INotificationsProvider
+    {
+        IEnumerable<NotificationInfo> GetNotifications(
+            SeekDirection direction = SeekDirection.Forward, 
+            IReadOnlySet<UInt160>? contracts = null, 
+            IReadOnlySet<string>? eventNames = null);
+    }
+}
