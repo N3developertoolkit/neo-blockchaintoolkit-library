ee924a372bfdaabcba6df9e5583cebb871a37cf6
Merl <37552183+merl111@users.noreply.github.com>
2022-12-09 19:34:25 +0100

    add callback to PrefetchAsync, fix #73, hotfix for neo-project/neo#2829 (#74)
    * add callback to PrefetchAsync, fix #73, hotfix for neo-project/neo#2829
    
    * Fix nullable parameters + update param order as per standard .NET guidance
    
    Co-authored-by: Harry Pierson <harrypierson@hotmail.com>
    Co-authored-by: Harry <harrypierson@outlook.com>

--- src/bctklib/persistence/StateServiceStore.cs
+++ src/bctklib/persistence/StateServiceStore.cs
@@ -3,6 +3,7 @@ using System.Buffers;
 using System.Buffers.Binary;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -108,7 +109,7 @@ namespace Neo.BlockchainToolkit.Persistence
             }
         }
 
-        public async Task<OneOf<Success, Error<string>>> PrefetchAsync(UInt160 contractHash, CancellationToken token)
+        public async Task<OneOf<Success, Error<string>>> PrefetchAsync(UInt160 contractHash, CancellationToken token, Action<RpcFoundStates>? callback = null)
         {
             var info = branchInfo.Contracts.Single(c => c.Hash == contractHash);
 
@@ -124,7 +125,7 @@ namespace Neo.BlockchainToolkit.Persistence
                         if (!cacheClient.TryGetCachedFoundStates(contractHash, prefixes[i], out var _))
                         {
                             anyPrefixDownloaded = true;
-                            await DownloadStatesAsync(contractHash, prefixes[i], token).ConfigureAwait(false);
+                            await DownloadStatesAsync(contractHash, prefixes[i], callback, token).ConfigureAwait(false);
                         }
                     }
 
@@ -145,7 +146,7 @@ namespace Neo.BlockchainToolkit.Persistence
                 }
                 else
                 {
-                    await DownloadStatesAsync(contractHash, null, token).ConfigureAwait(false);
+                    await DownloadStatesAsync(contractHash, null, callback, token).ConfigureAwait(false);
                     return default(Success);
                 }
             }
@@ -195,8 +196,16 @@ namespace Neo.BlockchainToolkit.Persistence
                         var (key, value) = found.Results[i];
                         if (key.AsSpan().StartsWith(prefix.Span))
                         {
-                            var state = new StorageItem(value).GetInteroperable<ContractState>();
-                            contracts.Add(new ContractInfo(state.Id, state.Hash, state.Manifest.Name));
+                            // Temporary fix --> https://github.com/neo-project/neo/issues/2829
+                            try
+                            {
+                                var state = new StorageItem(value).GetInteroperable<ContractState>();
+                                contracts.Add(new ContractInfo(state.Id, state.Hash, state.Manifest.Name));
+                            }
+                            catch
+                            {
+
+                            }
                         }
                     }
                     if (!found.Truncated || found.Results.Length == 0) break;
@@ -448,7 +457,7 @@ namespace Neo.BlockchainToolkit.Persistence
             throw new Exception("DownloadStates failed");
         }
 
-        async Task<int> DownloadStatesAsync(UInt160 contractHash, byte? prefix, CancellationToken token)
+        async Task<int> DownloadStatesAsync(UInt160 contractHash, byte? prefix, Action<RpcFoundStates>? callback, CancellationToken token)
         {
             const string loggerName = nameof(DownloadStatesAsync);
             Activity? activity = DownloadStatesStart(loggerName, contractHash, prefix);
@@ -464,8 +473,10 @@ namespace Neo.BlockchainToolkit.Persistence
                     token.ThrowIfCancellationRequested();
                     var found = await rpcClient.FindStatesAsync(branchInfo.RootHash, contractHash, prefixOwner.Memory, from).ConfigureAwait(false);
                     token.ThrowIfCancellationRequested();
+                    callback?.Invoke(found);
                     if (WriteFoundStates(found, snapshot, out from, ref count, loggerName)) break;
                 }
+                snapshot.Commit();
                 return count;
             }
             catch
